7dc32f5686496cc271c74bf1f35e3447049f3b36
larissacauane <larissa_cauane10@hotmail.com>
2022-08-24 10:54:53 -0300

    add desafio leap year

---
+++ exercism/codigo/leap-year.js
@@ -0,0 +1,13 @@
+function isLeapYear(year) {
+  return (
+    (year % 4 === 0 && year % 100 !== 0) ||
+    (year % 100 === 0 && year % 400 === 0)
+  );
+}
+
+console.log(isLeapYear(1997));
+console.log(isLeapYear(1996));
+console.log(isLeapYear(1900));
+console.log(isLeapYear(2000));
+console.log(isLeapYear(2022));
+console.log(isLeapYear(2024));
